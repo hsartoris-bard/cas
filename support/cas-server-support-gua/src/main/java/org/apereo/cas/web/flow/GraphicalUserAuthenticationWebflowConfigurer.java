f8cd995a6f9910de793bf2e08af2172f844de11f
Hayden Sartoris <hsartoris@bard.edu>
2019-12-18 16:49:48 -0500

    forgot import again

--- support/cas-server-support-gua/src/main/java/org/apereo/cas/web/flow/GraphicalUserAuthenticationWebflowConfigurer.java
+++ support/cas-server-support-gua/src/main/java/org/apereo/cas/web/flow/GraphicalUserAuthenticationWebflowConfigurer.java
@@ -7,6 +7,7 @@ import lombok.val;
 import org.springframework.context.ApplicationContext;
 import org.springframework.webflow.definition.registry.FlowDefinitionRegistry;
 import org.springframework.webflow.engine.ActionState;
+import org.springframework.webflow.engine.ViewState;
 import org.springframework.webflow.engine.Transition;
 import org.springframework.webflow.engine.builder.support.FlowBuilderServices;
 
